573c742eaa57b4651924e21b11bdf770074e3156
MiceliLanda <183381@ids.upchiapas.edu.mx>
2021-12-05 23:02:26 -0600

    Release v1.0

--- Project.py
+++ Project.py
@@ -1,49 +1,92 @@
-from concurrent.futures import ThreadPoolExecutor
-import requests
-import os, sys, time
+from threading import Thread
+import sys, os, time, requests, colorama
 cls = lambda: os.system('cls')
-directorios = []
-contador =0
+from queue import Queue
+from fake_useragent import UserAgent
+from colorama import Fore
+from colorama import Style
+colorama.init()
+concurrent = 50
+q = Queue(concurrent * 2)
 
-def recopilatorio(archivo, link):
-	valores = open(archivo, "r")
-	if os.path.exists(archivo):
-		for word in valores:
-			directorios.append(link+word.replace('\n',''))
-		valores.close()
-		return directorios
-	else:
-		print('No se ha encontrado el Fichero')
-	valores.close()
-	
-def proceso(urls):
-	global contador
-	res = requests.get(urls, timeout=2);
-	if res.status_code != 404:
-		contador+=1
-		print(f'   {os.getpid()}  url-> {urls}')
+def menu():
+    cls()
+    print(f"""{Fore.CYAN}
+ __                     _                    _             _  _    ___  _  _
+/ _\ ___  __ _ _ __ ___| |__   ___ _ __ /\ /(_)_ __   __ _| || |  / _ \| || |
+\ \ / _ \/ _` | '__/ __| '_ \ / _ \ '__/ //_/ | '_ \ / _` | || |_| | | | || |_
+_\ \  __/ (_| | | | (__| | | |  __/ | / __ \| | | | | (_| |__   _| |_| |__   _|
+\__/\___|\__,_|_|  \___|_| |_|\___|_| \/  \/|_|_| |_|\__, |  |_|  \___/   |_|
+                                                     |___/
+                                                           By: 183381,183423
+{Style.RESET_ALL}""")
+    print(f'{Fore.LIGHTMAGENTA_EX}[+] Comenzando ... Espere un momento por favor . . .\n{Style.RESET_ALL}')
+    global link,ruta
+    link = str(sys.argv[1])
+    ruta = os.getcwd()+sys.argv[2]
+
+def userAgent():
+    ua = UserAgent()
+    return ua.random
+
+def proceso():
+    global contador
+    contador = 0
+    while True:
+        url = link + q.get()
+        status, url = Peticiones(url)
+        directoriosEncontrados(status, url)
+        contador+=1
+        q.task_done()
+
+def Peticiones(ourl):
+    try:
+        res = requests.get(ourl, timeout=5)
+        return res.status_code, ourl
+    except:
+        return "Ha ocurrido un error, verifique la url.\n", ourl
+
+def directoriosEncontrados(status, url):
+    if status != 404:
+        if status == 200:
+            print(f'{Fore.LIGHTGREEN_EX}status: {status}  |  url: {url} {Style.RESET_ALL}')
+        elif status ==403:
+            print(f'{Fore.YELLOW}status: {status} Administrador  |  url: {url} {Style.RESET_ALL}')
+    
+def Fuzzing():
+    ua = userAgent()
+    for i in range(concurrent):
+        t = Thread(target=proceso)
+        t.daemon = True
+        t.start()
+    try:
+        valores = open(ruta, 'r')
+        for url in valores:
+            q.put(url.strip())
+        q.join()
+    except KeyboardInterrupt:
+        sys.exit(1)
 
 def main():
-	if len(sys.argv) < 4:
-		cls()
-		print(f'[*] Uso del programa...! \n\n nombrePrograma.py  url a escanear  "directorio diccionario"  #Threads')
-	else:
-		url = str(sys.argv[1])
-		ruta =os.getcwd()+sys.argv[2]
-		threads = int(sys.argv[3]) 	
-		cls()
-		print(f'[+] Comenzando ... Espere un momento por favor . . .\n')
-		recopilatorio(ruta, url)
-		if directorios:
-			start = time.perf_counter()
-			with ThreadPoolExecutor(max_workers=threads) as p:
-				p.map(proceso, directorios)
-			finish = time.perf_counter()
-			print(f"Fuzzing terminado en {round(finish-start, 2)} segundos\n")
-			print('\nDirectorios encontrados : ', contador)
-		else:
-			print('Ha ocurrido un error')
+    menu()
+    start = time.perf_counter()
+    Fuzzing()
+    finish = time.perf_counter()
+    print(f"\n{Fore.LIGHTCYAN_EX}Fuzzing terminado en {round(finish-start, 2)} segundos\n")
+    print(f'{Fore.LIGHTYELLOW_EX}Directorios probados : ', contador)
 
 if __name__ == '__main__':
-
-    main()  
+    if len(sys.argv) < 3:
+        cls()
+        print(f"""{Fore.LIGHTGREEN_EX}
+ __                     _                    _             _  _    ___  _  _
+/ _\ ___  __ _ _ __ ___| |__   ___ _ __ /\ /(_)_ __   __ _| || |  / _ \| || |
+\ \ / _ \/ _` | '__/ __| '_ \ / _ \ '__/ //_/ | '_ \ / _` | || |_| | | | || |_
+_\ \  __/ (_| | | | (__| | | |  __/ | / __ \| | | | | (_| |__   _| |_| |__   _|
+\__/\___|\__,_|_|  \___|_| |_|\___|_| \/  \/|_|_| |_|\__, |  |_|  \___/   |_|
+                                                     |___/
+                                                           By: 183381,183423
+{Style.RESET_ALL}""")
+        print(f'{Fore.LIGHTYELLOW_EX}[*] Uso del programa...! \n\n nombrePrograma.py  <url>   <diccionario> {Style.RESET_ALL}')
+    else:
+        main()
--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Project_SearcherKing404
 
 183381 - Miceli Landa Jhonathan de Jesús
-
 183423 - Ballinas Trinidad Luis Eduardo
 
 [+] Uso del programa ...!
 
-name.py 'url'  'ruta diccionario'  #Threads 
+python <name.py>  <url>  <diccionario> 
